3b2ace888ebe8c056de62d1ec60135064ddbadd0
Aldrin Montana <octalene.dev@pm.me>
2023-01-31 13:12:05 -0800

    mohair/skytether: updated import

--- mohair/skytether/algebra_pb2.py
+++ mohair/skytether/algebra_pb2.py
@@ -12,8 +12,8 @@ _sym_db = _symbol_database.Default()
 
 
 from google.protobuf import any_pb2 as google_dot_protobuf_dot_any__pb2
-from substrait.extensions import extensions_pb2 as substrait_dot_extensions_dot_extensions__pb2
-from substrait import type_pb2 as substrait_dot_type__pb2
+from mohair.substrait.extensions import extensions_pb2 as substrait_dot_extensions_dot_extensions__pb2
+from mohair.substrait import type_pb2 as substrait_dot_type__pb2
 
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17skytether/algebra.proto\x12\tskytether\x1a\x19google/protobuf/any.proto\x1a%substrait/extensions/extensions.proto\x1a\x14substrait/type.proto\"V\n\x06SkyRel\x12\x16\n\x06\x64omain\x18\x01 \x01(\tR\x06\x64omain\x12\x1c\n\tpartition\x18\x02 \x01(\tR\tpartition\x12\x16\n\x06slices\x18\x03 \x03(\rR\x06slices\"Z\n\nKineticRel\x12\x16\n\x06\x64omain\x18\x01 \x01(\tR\x06\x64omain\x12\x1c\n\tpartition\x18\x02 \x01(\tR\tpartition\x12\x16\n\x06slices\x18\x03 \x03(\rR\x06slicesb\x06proto3')
