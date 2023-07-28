94d608fb57254c59398880c09ed57d25aa70ad06
Aldrin Montana <octalene.dev@pm.me>
2023-07-28 14:16:14 -0700

    task: moved substrait-related code to a module
    This is to facilitate an insulating layer from the substrait library's
    interface. This commit also includes code to uptake the new
    `DecomposedPlan` and uses the new `MohairTo` set of functions
    (single dispatch) that translates from a `MohairOp` to a substrait
    `Rel`.
    
    Issue: #22

--- toolbox/to_mohair.py
+++ toolbox/to_mohair.py
@@ -4,35 +4,45 @@
 # >> Standard modules
 from pathlib import Path
 
-from mohair.query.planner import ParseSubstrait, ParseQueryPlan
+# >> Mohair
+from mohair import CreateMohairLogger
+
+from mohair.query.substrait import SubstraitPlan
 from mohair.query.planner import ViewQueryPlan, ViewPlanOp, ViewQueryBreakers
 from mohair.query.planner import SplitQueryPlan
-
-from mohair import CreateMohairLogger
 
 # ------------------------------
 # Module variables
 
 logger = CreateMohairLogger('Main')
 
-example_input = Path('resources') / 'examples' / 'average-expression.substrait'
+example_input  = Path('resources') / 'examples' / 'average-expression.substrait'
+example_output = Path('resources') / 'examples' / 'average-expression.subplan.substrait'
 
+
+# ------------------------------
+# Main Logic
 
 if __name__ == '__main__':
     # parse the protobuf into a Mohair query plan
-    with open(example_input, 'rb') as file_handle:
-        substrait_plan = ParseSubstrait(file_handle.read())
+    with open(example_input, 'rb') as query_handle:
+        substrait_plan = SubstraitPlan.FromMessageBytes(query_handle.read())
 
     # construct and view the mohair structure
-    mohair_plan = ParseQueryPlan(substrait_plan)
+    mohair_plan = substrait_plan.ToMohair()
     print(ViewQueryPlan(mohair_plan))
 
     # view the breakers in the mohair plan
     print(ViewQueryBreakers(mohair_plan))
 
     # View a naive subplan
-    super_plan, sub_plans = SplitQueryPlan(mohair_plan)
+    decomposed_plan = SplitQueryPlan(mohair_plan)
 
     logger.info('SubPlans:')
-    for sub_plan in sub_plans:
-        logger.info(ViewPlanOp(sub_plan.query_subplan))
+    for subplan_root in decomposed_plan.anchor_subplans:
+        logger.info(ViewPlanOp(subplan_root))
+
+    subplan_tree_ndx  = 0
+    substrait_subplan = substrait_plan.ToSubPlanMessage(decomposed_plan, subplan_tree_ndx)
+    with open(example_output, 'wb') as subplan_handle:
+        subplan_handle.write(substrait_subplan.msg)
