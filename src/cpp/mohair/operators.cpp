eae2ecefaeaf922e2124f2c2211e04c948a6ef0b
Aldrin Montana <octalene.dev@pm.me>
2023-09-20 10:08:40 -0700

    task: major updates to mohair plan modeling
    I was previously trying to use std::variant to represent the types of
    input ops a query op could have, which was real silly. This re-designs
    to actually use polymorphism but loses out on empty class optimization
    (which was something I wanted to try). Some possible optimizations in
    code architecture may be lost with this approach but at least it's
    functional.

--- src/cpp/mohair/operators.cpp
+++ src/cpp/mohair/operators.cpp
@@ -28,87 +28,180 @@
 
 namespace mohair {
 
-  // >> Accessors for OpVariant that use visitors
-  string OpVariant::table_name() {
-    // auto& rel_op should be a unique_ptr&
-    auto tname_visitor = [](const auto& rel_op) { return rel_op->table_name; }
+  // >> Implementations to complete QueryOp, PipelineOp, and BreakerOp types
+  unique_ptr<PlanAnchor> QueryOp::plan_anchor() { return nullptr;          }
+  string                 QueryOp::ToString()    { return table_name;       }
+  const string           QueryOp::ViewStr()     { return this->ToString(); }
 
-    return std::visit(tname_visitor, this->op);
+  string                 PipelineOp::ToString() { return table_name;       }
+  string                 BreakerOp::ToString()  { return table_name;       }
+
+  // >> Implementations for each op type to return its PlanAnchor
+  unique_ptr<PlanAnchor> PlanAnchorForRel(Rel *anchor_relmsg) {
+    // wrap Rel in a new PlanAnchor message
+    auto anchor_msg = std::make_unique<PlanAnchor>();
+    anchor_msg->set_allocated_anchor_rel(anchor_relmsg);
+
+    // Then return the constructed message
+    return anchor_msg;
   }
 
-  const Rel* OpVariant::op_wrap() {
-    // auto& rel_op should be a unique_ptr&
-    auto wrap_visitor = [](const auto& rel_op) { return rel_op->op_wrap; }
+  unique_ptr<PlanAnchor> OpProj::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_project()->clear_input();
 
-    return std::visit(wrap_visitor, this->op);
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
   }
 
-  unique_ptr<PlanAnchor> OpVariant::plan_anchor() {
-    return std::visit(OpVariant::PlanAnchorVisitor, this->op);
+  unique_ptr<PlanAnchor> OpSel::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_filter()->clear_input();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
   }
 
+  unique_ptr<PlanAnchor> OpLimit::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_fetch()->clear_input();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpSort::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_sort()->clear_input();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpAggr::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_aggregate()->clear_input();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpCrossJoin::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_cross()->clear_left();
+    simplified_rel->mutable_cross()->clear_right();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpJoin::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_join()->clear_left();
+    simplified_rel->mutable_join()->clear_right();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpHashJoin::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_hash_join()->clear_left();
+    simplified_rel->mutable_hash_join()->clear_right();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  unique_ptr<PlanAnchor> OpMergeJoin::plan_anchor() {
+    // copy the Rel message and simplify its content (we don't need its full tree)
+    auto simplified_rel = std::make_unique<Rel>(*(this->op_wrap));
+    simplified_rel->mutable_merge_join()->clear_left();
+    simplified_rel->mutable_merge_join()->clear_right();
+
+    // Then return the PlanAnchor message
+    return PlanAnchorForRel(simplified_rel.release());
+  }
+
+  // >> End of plan_anchor() implementations
 
   // >> Specific translation functions (from Substrait to Mohair)
   template <typename UnaryRelMsg, typename MohairRel>
-  OpVariant FromUnaryOpMsg(Rel *rel_msg, UnaryRelMsg *substrait_op) {
+  unique_ptr<QueryOp> FromUnaryOpMsg(Rel *rel_msg, UnaryRelMsg *substrait_op) {
     // recurse on input relation
-    auto op_input = MohairFrom(substrait_op->mutable_input());
+    unique_ptr<QueryOp> op_input = MohairFrom(substrait_op->mutable_input());
 
-    // rvals for constructor args
-    tuple<OpVariant> &&op_inputs = { op_input };
-    auto             &&op_tname  = op_input->table_name();
+    // Initialize op and set its inputs
+    auto op_tname = string { op_input->table_name };
+    auto unary_op = std::make_unique<MohairRel>(
+      substrait_op, rel_msg, std::move(op_tname)
+    );
+    get<0>(unary_op->op_inputs) = std::move(op_input);
 
-    // inline construction of  OpVariant (variant wrapper around a unique pointer)
-    return OpVariant { 
-       std::in_place_type<unique_ptr<MohairRel>>
-      ,std::make_unique<MohairRel>(substrait_op, rel_msg, op_inputs, op_tname)
-    };
+    return unary_op;
   }
 
   template <typename BinaryRelMsg, typename MohairRel>
-  OpVariant FromBinaryOpMsg(Rel *rel_msg, BinaryRelMsg *substrait_op) {
+  unique_ptr<QueryOp> FromBinaryOpMsg(Rel *rel_msg, BinaryRelMsg *substrait_op) {
     // recurse on input relations
-    auto left_input  = MohairFrom(substrait_op->mutable_left() );
-    auto right_input = MohairFrom(substrait_op->mutable_right());
+    unique_ptr<QueryOp> left_input  = MohairFrom(substrait_op->mutable_left() );
+    unique_ptr<QueryOp> right_input = MohairFrom(substrait_op->mutable_right());
 
-    // rvals for constructor args
-    tuple<OpVariant, OpVariant> &&op_inputs = { left_input, right_input };
-    auto &&op_tname = left_input->table_name() + "." + right_input->table_name();
+    // prep params for the operator
+    auto op_tname = string { left_input->table_name + "." + right_input->table_name };
+    auto binary_op = std::make_unique<MohairRel>(
+      substrait_op, rel_msg, std::move(op_tname)
+    );
 
-    // inline construction of  OpVariant (variant wrapper around a unique pointer)
-    return OpVariant {
-       std::in_place_type<unique_ptr<MohairRel>>
-      ,std::make_unique<MohairRel>(substrait_op, rel_msg, op_inputs, op_tname)
-    };
+    get<0>(binary_op->op_inputs) = std::move(left_input);
+    get<1>(binary_op->op_inputs) = std::move(right_input);
+
+    return binary_op;
   }
 
 
-  OpVariant FromReadMsg(Rel *rel_msg, ReadRel *substrait_op) {
-    string &&op_tname = std::move(SourceNameForRead(substrait_op));
+  unique_ptr<QueryOp> FromReadMsg(Rel *rel_msg, ReadRel *substrait_op) {
+    // prep params for the operator
+    string  op_tname  = SourceNameForRead(substrait_op);
 
-    return OpVariant {
-       std::in_place_type<unique_ptr<OpRead>>
-      ,std::make_unique<OpRead>(substrait_op, rel_msg, {}, op_tname)
-    };
+    // construct the operator
+    unique_ptr<QueryOp> read_op = std::make_unique<OpRead>(
+      substrait_op, rel_msg, std::move(op_tname)
+    );
+
+    return read_op;
   }
 
 
-  OpVariant FromSkyMsg(Rel *rel_msg, SkyRel *substrait_op) {
+  /*
+  unique_ptr<QueryOp> FromSkyMsg(Rel *rel_msg, unique_ptr<SkyRel> substrait_op) {
     // TODO: implementation
     // TODO: validate inline construction of unique pointer
-    return OpVariant {
-       std::in_place_type<unique_ptr<OpSkyRead>>
-      ,unique_ptr<OpSkyRead>{}
-    };
+    if (substrait_op == nullptr) {
+      auto sky_op = std::make_unique<OpSkyRead>();
+      return sky_op;
+    }
+
+    return OpVariant {};
+    // return OpVariant { CreateRelVariant<OpSkyRead>(std::move(substrait_op)) };
   }
+  */
 
 
   /**
    * A function to convert a substrait `Rel` message to a Mohair `QueryOp` derived
    * class.
    */
-  OpVariant MohairFrom(const Rel *rel_msg) {
-    switch(rel_msg.rel_type_case()) {
+  unique_ptr<QueryOp> MohairFrom(Rel *rel_msg) {
+    switch(rel_msg->rel_type_case()) {
       // Translate pipeline-able operators
       case Rel::RelTypeCase::kProject: {
         return FromUnaryOpMsg<ProjectRel, OpProj>(rel_msg, rel_msg->mutable_project());
@@ -150,53 +243,53 @@ namespace mohair {
       case Rel::RelTypeCase::kRead: {
         return FromReadMsg(rel_msg, rel_msg->mutable_read());
       }
-      case Rel::RelTypeCase::kExtensionLeaf: {
-        return FromSkyMsg(rel_msg, rel_msg->mutable_extension_leaf());
-      }
+      case Rel::RelTypeCase::kExtensionLeaf:
+      /* TODO
+      {
+        substrait::ExtensionLeafRel *leaf_rel = rel_msg->mutable_extension_leaf();
+        if (leaf_rel->has_detail()) {
+          auto sky_rel = std::make_unique<SkyRel>();
+          leaf_rel->detail().UnpackTo(sky_rel.get());
 
-      // note: may not be pipeline breaking, but we can improve that when we get there
-      /* TODO: figure out how to do variable length tuple later
-      case Rel::RelTypeCase::kSet: {
-        return MohairFrom(rel_msg.set());
+          return FromSkyMsg(rel_msg, sky_rel);
+        }
+
+        return FromSkyMsg(rel_msg, nullptr);
       }
       */
 
       // Catch all error
       default: {
-        return OpVariant {
-           std::in_place_type<unique_ptr<OpErr>>
-          ,std::make_unique<OpErr>(rel_msg, "ParseError: Unknown substrait operator")
-        };
+        // construct error here since it's simple
+        return std::make_unique<OpErr>(
+          rel_msg, "ParseError: Unknown substrait operator"
+        );
       }
     }
   }
 
   // >> Translation from Mohair to Substrait
-  const Rel* SubstraitFrom(OpVariant& mohair_op) {
-    return mohair_op.op_wrap();
+  unique_ptr<PlanAnchor> PlanAnchorFrom(unique_ptr<QueryOp> &mohair_op) {
+    return mohair_op->plan_anchor();
   }
 
-  unique_ptr<PlanAnchor> PlanAnchorFrom(OpVariant& mohair_op) {
-    return mohair_op.plan_anchor();
-  }
-
-  string SourceNameForRead(const ReadRel *substrait_op) {
+  string SourceNameForRead(ReadRel *substrait_op) {
     switch (substrait_op->read_type_case()) {
       case ReadRel::ReadTypeCase::kNamedTable: {
         std::stringstream tname_stream;
-        const auto src_table = substrait_op->named_table();
+        auto src_table = substrait_op->named_table();
 
         tname_stream << src_table.names(0);
         for (int tname_ndx = 1; tname_ndx < src_table.names_size(); ++tname_ndx) {
           tname_stream << "." << src_table.names(tname_ndx);
         }
 
-        return tname_stream.str();
+        return string { tname_stream.str() };
       }
 
       // all other cases can get a default name for now
       default: {
-        return "ReadRel";
+        return string { "ReadRel" };
       }
     }
   }
--- src/cpp/mohair/operators.hpp
+++ src/cpp/mohair/operators.hpp
@@ -28,8 +28,8 @@
 // >> Internal libs
 #include "../headers/mohair.hpp"
 
-// >> Type Aliases
 
+// >> Type Aliases
 //  |> For standard libs
 using std::tuple;
 using std::get;
@@ -64,256 +64,149 @@ namespace mohair {
 
   // >> Leaf operators
   struct OpErr : QueryOp {
-    const Rel *plan_op;
-    string     err_msg;
+    string err_msg;
+
+    OpErr(Rel *rel, const char *msg)
+      : QueryOp(rel, string {""}), err_msg(msg) {}
   };
 
   struct OpRead : PipelineOp {
-    const ReadRel *plan_op;
-    const Rel     *op_wrap;
-    tuple<>        op_inputs;
-    string         table_name;
+    ReadRel *plan_op;
+
+    OpRead(ReadRel *op, Rel *rel, string tname)
+      : PipelineOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override { return nullptr; }
   };
 
   // TODO: figure out how this should bridge to skytether
   struct OpSkyRead : PipelineOp {
-    const SkyRel *plan_op;
-    const Rel    *op_wrap;
-    tuple<>       op_inputs;
-    string        table_name;
+    SkyRel  *plan_op;
+
+    unique_ptr<PlanAnchor> plan_anchor() override { return nullptr; }
   };
 
-  // >> Pipeline-able operators
-  template <typename InputType>
+  // >> Complete definitions of query operators
+  //  |> Pipeline-able operators
   struct OpProj : PipelineOp {
-    const ProjectRel *plan_op;
-    const Rel        *op_wrap;
-    tuple<OpVariant>  op_inputs;
-    string            table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>>;
+
+    ProjectRel *plan_op;
+    InputsType  op_inputs;
+
+    OpProj(ProjectRel *op, Rel *rel, string tname)
+      : PipelineOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpSel : PipelineOp {
-    const FilterRel  *plan_op;
-    const Rel        *op_wrap;
-    tuple<OpVariant>  op_inputs;
-    string            table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>>;
+
+    FilterRel  *plan_op;
+    InputsType  op_inputs;
+
+    OpSel(FilterRel *op, Rel *rel, string tname)
+      : PipelineOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpLimit : PipelineOp {
-    const FetchRel   *plan_op;
-    const Rel        *op_wrap;
-    tuple<OpVariant>  op_inputs;
-    string            table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>>;
+
+    FetchRel   *plan_op;
+    InputsType  op_inputs;
+
+    OpLimit(FetchRel *op, Rel *rel, string tname)
+      : PipelineOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
 
-  // >> Pipeline breaking operators
-  template <typename InputType>
+  //  |> Pipeline breaking operators
   struct OpSort : BreakerOp {
-    const SortRel    *plan_op;
-    const Rel        *op_wrap;
-    tuple<OpVariant>  op_inputs;
-    string            table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>>;
+
+    SortRel    *plan_op;
+    InputsType  op_inputs;
+
+    OpSort(SortRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpAggr : BreakerOp {
-    const AggregateRel *plan_op;
-    const Rel          *op_wrap;
-    tuple<OpVariant>    op_inputs;
-    string              table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>>;
+
+    AggregateRel *plan_op;
+    InputsType    op_inputs;
+
+    OpAggr(AggregateRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpCrossJoin : BreakerOp {
-    const CrossRel              *plan_op;
-    const Rel                   *op_wrap;
-    tuple<OpVariant, OpVariant>  op_inputs;
-    string                       table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>, unique_ptr<QueryOp>>;
+
+    CrossRel   *plan_op;
+    InputsType  op_inputs;
+
+    OpCrossJoin(CrossRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpJoin : BreakerOp {
-    const JoinRel               *plan_op;
-    const Rel                   *op_wrap;
-    tuple<OpVariant, OpVariant>  op_inputs;
-    string                       table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>, unique_ptr<QueryOp>>;
+
+    JoinRel    *plan_op;
+    InputsType  op_inputs;
+
+    OpJoin(JoinRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpHashJoin : BreakerOp {
-    const HashJoinRel           *plan_op;
-    const Rel                   *op_wrap;
-    tuple<OpVariant, OpVariant>  op_inputs;
-    string                       table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>, unique_ptr<QueryOp>>;
+
+    HashJoinRel *plan_op;
+    InputsType   op_inputs;
+
+    OpHashJoin(HashJoinRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
-  template <typename InputType>
   struct OpMergeJoin : BreakerOp {
-    const MergeJoinRel          *plan_op;
-    const Rel                   *op_wrap;
-    tuple<OpVariant, OpVariant>  op_inputs;
-    string                       table_name;
+    using InputsType = tuple<unique_ptr<QueryOp>, unique_ptr<QueryOp>>;
+
+    MergeJoinRel                                    *plan_op;
+    InputsType  op_inputs;
+
+    OpMergeJoin(MergeJoinRel *op, Rel *rel, string tname)
+      : BreakerOp(rel, tname), plan_op(op) {}
+
+    unique_ptr<PlanAnchor> plan_anchor() override;
   };
 
   /* TODO: needs variadic op_inputs
   struct OpSet : BreakerOp {
-    SetRel                *plan_op;
-    Rel                   *op_wrap;
-    tuple<OpVariant, ...>  op_inputs;
-    unique_ptr<string>     table_name;
+    SetRel                          *plan_op;
+    tuple<unique_ptr<QueryOp>, ...>  op_inputs;
   };
   */
 
 
-  // >> Wrapper around operator variant
-  struct OpVariant {
-
-    // Enum of operator types that may be stored
-    enum VariantKind {
-      ERR  ,
-      READ , SKY_READ,
-      PROJ , SEL     , LIMIT,
-      SORT , AGGR    ,
-      CROSS, JOIN    ,
-      HASHJ, MERGEJ  ,
-      SET  ,
-    };
-
-    // Type alias for a variant
-    using VariantType = std::variant<
-      unique_ptr<OpErr>      ,
-      unique_ptr<OpRead>     , unique_ptr<OpSkyRead>  ,
-      unique_ptr<OpProj>     , unique_ptr<OpSel>      , unique_ptr<OpLimit>,
-      unique_ptr<OpSort>     , unique_ptr<OpAggr>     ,
-      unique_ptr<OpCrossJoin>, unique_ptr<OpJoin>     ,
-      unique_ptr<OpHashJoin> , unique_ptr<OpMergeJoin>
-      // unique_ptr<OpSet>
-    >;
-
-    // Declaration of member variable of variant type
-    VariantType op;
-
-    // Convenience functions (the purpose of this wrapper)
-    OpVariant::VariantKind kind() const {
-      switch (this->op.index()) {
-        case  1: return OpVariant::READ;
-        case  2: return OpVariant::SKY_READ;
-        case  3: return OpVariant::PROJ;
-        case  4: return OpVariant::SEL;
-        case  5: return OpVariant::LIMIT;
-        case  6: return OpVariant::SORT;
-        case  7: return OpVariant::AGGR;
-        case  8: return OpVariant::CROSS;
-        case  9: return OpVariant::JOIN;
-        case 10: return OpVariant::HASHJ;
-        case 11: return OpVariant::MERGEJ;
-        case 12: return OpVariant::SET;
-        case  0:
-        default:
-          return OpVariant::ERR;
-      }
-    }
-
-    struct PlanAnchorVisitor {
-      /**
-        * Construct a PlanAnchor using the `Rel` parameter.
-        */
-      unique_ptr<PlanAnchor> PlanAnchorForRel(Rel *anchor_relmsg) {
-        auto anchor_msg = std::make_unique<PlanAnchor>();
-        anchor_msg.set_allocated_anchor_rel(anchor_relmsg);
-
-        return anchor_msg;
-      }
-
-      // >> Visitor functions for each type of mohair operator
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpRead>& op) {
-        return PlanAnchorForRel(SubstraitFrom(*op));
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpSkyRead>& op) {
-        return PlanAnchorForRel(SubstraitFrom(*op));
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpProj>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->project().clear_input();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpSel>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->filter().clear_input();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpLimit>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->fetch().clear_input();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpSort>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->sort().clear_input();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpAggr>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->aggregate().clear_input();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpCrossJoin>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->cross().clear_left();
-        anchor_relmsg->cross().clear_right();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpJoin>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->join().clear_left();
-        anchor_relmsg->join().clear_right();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpHashJoin>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->hash_join().clear_left();
-        anchor_relmsg->hash_join().clear_right();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-      unique_ptr<PlanAnchor> operator()(unique_ptr<OpMergeJoin>& op) {
-        auto anchor_relmsg = SubstraitFrom(*op);
-        anchor_relmsg->merge_join().clear_left();
-        anchor_relmsg->merge_join().clear_right();
-
-        return PlanAnchorForRel(anchor_relmsg);
-      }
-
-    }; // struct PlanAnchorVisitor
-
-    // >> Accessors for OpVariant that use visitors
-    string table_name();
-    const Rel* op_wrap();
-    unique_ptr<PlanAnchor> plan_anchor();
-
-  }; // struct OpVariant
-
   // >> Convenience functions
-  string SourceNameForRead(const ReadRel *substrait_op);
+  string SourceNameForRead(ReadRel *substrait_op);
 
 } // namespace: mohair
