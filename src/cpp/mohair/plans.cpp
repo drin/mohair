f75379910555b1644fc51ff4954668488866b65e
Aldrin Montana <octalene.dev@pm.me>
2023-12-08 16:52:29 -0800

    task: refactored logic to fix memory management
    It was difficult to move vector<unique_ptr> between callers and callees
    with the previous approach. This changes the approach to make
    accumulating pointers to pipeline breaking query operators much more
    straightforward and fixes the issue of AppPlans being deconstructed too
    early.

--- src/cpp/mohair/plans.cpp
+++ src/cpp/mohair/plans.cpp
@@ -107,146 +107,87 @@ namespace mohair {
 
     // find the spot to insert
     for (; plans_it != plans.end(); plans_it = std::next(plans_it)) {
-      if (new_plan->attrs->pipeline_len > (*plans_it)->attrs->pipeline_len) { break; }
+      if (new_plan->attrs.pipe_len > (*plans_it)->attrs.pipe_len) { break; }
     }
 
     // we always insert, even if its at the end of the list
     plans.insert(plans_it, std::move(new_plan));
   }
 
-  // >> DecomposableProperties functions
-  string DecomposableProperties::ToString() {
+  // >> PlanAttrs functions
+  string PlanAttrs::ToString() {
     std::stringstream prop_stream;
 
     prop_stream << "Plan properties:"   << std::endl
-                << "\tPipeline length:" << pipeline_len   << std::endl
-                << "\tPlan width     :" << plan_width     << std::endl
-                << "\tPlan height    :" << plan_height    << std::endl
-                << "\tBreaker height :" << breaker_height << std::endl
+                << "\tPipeline length:" << pipe_len     << std::endl
+                << "\tPlan width     :" << plan_width   << std::endl
+                << "\tPlan height    :" << plan_height  << std::endl
+                << "\tBreaker height :" << break_height << std::endl
     ;
 
     return prop_stream.str();
   }
 
-  // PlanVec is a vector of unique_ptr<AppPlan>
-  unique_ptr<DecomposableProperties> PropertiesForPlanInputs(PlanVec &plans) {
-    auto properties = std::make_unique<DecomposableProperties>();
-
-    // pipeline length, plan width and height, and breaker height
-    int p_len = 0, pl_width = 0, pl_height = 0, bheight = 0;
-
-    // local references to vectors
-    std::vector<AppPlan*> *break_ops = &(properties->breakers);
-    std::vector<AppPlan*> *bleaf_ops = &(properties->breaker_leaves);
-
-    for (size_t ndx = 0; ndx < plans.size(); ++ndx) {
-      // >> grab a convenient reference to each plan; these are non-owning
-      AppPlan *plan = plans[ndx].get();
-
-      // >> pull in properties from the current plan input
-      std::vector<AppPlan*> *plan_breakops = &(plan->attrs->breakers);
-      std::vector<AppPlan*> *plan_bleafops = &(plan->attrs->breaker_leaves);
-
-      int plan_plwidth  = plan->attrs->plan_width;
-      int plan_plheight = plan->attrs->plan_height;
-      int plan_bheight  = plan->attrs->breaker_height;
-
-      int plan_plen = 0;
-      if (not plan->plan_op->IsBreaker()) { plan_plen = plan->attrs->pipeline_len; }
-
-      // >> update local variables
-      //   |> pointers for fast access
-      //      (put this first because it relates to the above check)
-      else if (plan->plan_op->IsBreaker()) {
-        // A breaker with no leaves must be a leaf itself
-        if (plan->attrs->breaker_leaves.size() == 0) {
-          bleaf_ops->push_back(plan);
-          // bleaf_ops->push_back(std::move(plans[ndx]));
-        }
-
-        // Otherwise, it is an internal breaker and we preserve its pointers
-        else {
-          bleaf_ops->insert(bleaf_ops->cend(), plan_bleafops->cbegin(), plan_bleafops->cend());
-          break_ops->insert(break_ops->cend(), plan_breakops->cbegin(), plan_breakops->cend());
-          break_ops->push_back(plan);
-          // break_ops->push_back(std::move(plans[ndx]));
-        }
-      }
-
-      //   |> max-type properties
-      if (plan_plen     > p_len    ) { p_len     = plan_plen;     }
-      if (plan_plheight > pl_height) { pl_height = plan_plheight; }
-      if (plan_bheight  > bheight)   { bheight   = plan_bheight;  }
-
-      //   |> sum-type properties
-      pl_width += plan_plwidth;
-    }
-
-    // >> non-conditional adjustments to properties
-    ++p_len;
-    ++pl_height;
-
-    // >> conditional adjustments to properties
-    if (pl_width == 0) { pl_width = 1; }
-
-    // >> update properties container and return it
-    properties->pipeline_len   = p_len;
-    properties->plan_height    = pl_height;
-    properties->plan_width     = pl_width;
-    properties->breaker_height = bheight;
-
-    return properties;
-  }
 
   // >> AppPlan static functions
-  std::vector<unique_ptr<AppPlan>> WalkInputOps(QueryOp *parent_op) {
-    // Gather child operators
-    std::vector<QueryOp *> child_ops = parent_op->GetOpInputs();
 
-    // Initialize a vector of AppPlan instances (wraps QueryOp with tree properties)
-    std::vector<unique_ptr<AppPlan>> sorted_plans;
-    sorted_plans.reserve(child_ops.size());
+  /**
+   * Walks a QueryOp DAG, propagating attributes from the leaves upwards in a depth-first
+   * order. When a pipeline breaking operation is encountered (e.g. Join or Aggregate),
+   * then the corresponding PlanAttrs is moved into an AppPlan and inserted into either
+   * break_ops or bleaf_ops.
+   */
+  PlanAttrs WalkPlanForDiscovery(QueryOp* parent_op, PlanVec& break_ops, PlanVec& bleaf_ops) {
+    // Base case: if there are no input ops, then return an empty PlanAttrs
+    auto child_ops = parent_op->GetOpInputs();
+    if (child_ops.size() == 0) { return PlanAttrs{}; }
 
-    if (child_ops.size() == 0) { return sorted_plans; }
+    // Variables to propagate attributes without a PlanAttrs instance
+    int p_len = 0, pl_width = 0, pl_height = 0, b_height = 0;
+    for (size_t op_ndx = 0; op_ndx < child_ops.size(); ++op_ndx) {
+      // Recurse on this op, then we can update properties
+      auto child_op     = child_ops[op_ndx];
+      auto child_attrs  = WalkPlanForDiscovery(child_op, break_ops, bleaf_ops);
 
-    // Recurse on first child operator; populate first element of sorted_plans
-    auto first_childplan = FromPlanOp(child_ops[0]);
-    sorted_plans.push_back(std::move(first_childplan));
+      // Update local variables
+      b_height   = std::max(b_height , child_attrs.break_height);
+      pl_height  = std::max(pl_height, child_attrs.plan_height );
+      pl_width  += child_attrs.plan_width;
 
-    // For each input operator to parent_op
-    for (size_t child_ndx = 1; child_ndx < child_ops.size(); ++child_ndx) {
-      // recurse on child operator
-      auto child_plan = FromPlanOp(child_ops[child_ndx]);
+      if (not child_op->IsBreaker()) { p_len = std::max(p_len, child_attrs.pipe_len); }
 
-      // insertion sort into sorted_plans
-      InsertionSortAppPlans(sorted_plans, std::move(child_plan));
+      // Update indices to breakers
+      else if (child_op->IsBreaker()) {
+        auto is_bleaf   = child_attrs.break_height == 1;
+        auto child_plan = std::make_unique<AppPlan>(child_op, child_attrs);
+        if (is_bleaf) { InsertionSortAppPlans(bleaf_ops, std::move(child_plan)); }
+        else          { InsertionSortAppPlans(break_ops, std::move(child_plan)); }
+      }
     }
 
-    // return AppPlan instances sorted by pipeline length (height-related property)
-    return sorted_plans;
+    // Return attribute struct (with some value adjustments)
+    if (parent_op->IsBreaker()) { ++b_height; }
+    return PlanAttrs { p_len + 1, pl_width, pl_height + 1, b_height };
+
+    /*
+    PlanAttrs p_attrs;
+    p_attrs.pipe_len     = p_len     + 1;
+    p_attrs.plan_height  = pl_height + 1;
+    p_attrs.break_height = b_height;
+    if (pl_width > 0) { p_attrs.plan_width = pl_width; }
+    return p_attrs;
+    */
   }
 
   unique_ptr<AppPlan> FromPlanOp(QueryOp *op) {
-    // Recurse on input operators
-    auto child_plans = WalkInputOps(op);
-
-    /*
-    if (   child_plans.size() == 2 && child_plans[0]->plan_op->IsBreaker() && child_plans[0]->plan_op->IsBreaker()) {
-      std::cout << "First case of error" << std::endl;
-    }
-    */
-
     // Create a plan that wraps this operator
     auto root_plan = std::make_unique<AppPlan>(op);
 
-    // Assign initial properties to root_plan
-    root_plan->attrs = PropertiesForPlanInputs(child_plans);
+    // Recurse on input operators
+    root_plan->attrs = WalkPlanForDiscovery(
+      op, root_plan->break_ops, root_plan->bleaf_ops
+    );
 
-    // Update properties if this operator is a pipeline breaker
-    if (op->IsBreaker()) { root_plan->attrs->breaker_height += 1; }
-
-    // Done with traversal
-    // NOTE: it seems like this gets called multiple times?
     return root_plan;
   }
 
