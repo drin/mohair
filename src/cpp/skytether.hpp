563171f13ace77f4829e6a5db682e955448ebcf1
Aldrin Montana <octalene.dev@pm.me>
2025-02-21 16:28:50 -0800

    task: made perf logging available in release mode

--- src/cpp/skytether.hpp
+++ src/cpp/skytether.hpp
@@ -36,40 +36,41 @@
 // ------------------------------
 // Macros
 
+#define SkytetherInitLogger(logger_name) {                              \
+  *(SkytetherLogger(logger_name)) << "Logger initialized" << std::endl; \
+}
+
+#define SkytetherStartTS(phase_name) \
+  SteadyTS ts_start_##phase_name = steady_clock::now();
+
+#define SkytetherStopTS(phase_name) \
+  SteadyTS ts_stop_##phase_name = steady_clock::now();
+
+#define SkytetherLogTimestamps(phase_name) {                                           \
+  auto ts_diff = mohair::StringifyTSDiff(ts_start_##phase_name, ts_stop_##phase_name); \
+  *(SkytetherLogger()) << "["                                                          \
+                            << mohair::StringifyTS(ts_start_##phase_name) << ":µs"     \
+                    << ", " << mohair::StringifyTS(ts_stop_##phase_name)  << ":µs"     \
+                    << ", " << ts_diff                                    << ":µs"     \
+                 << "] |> " << #phase_name << std::endl                                \
+  ;                                                                                    \
+}
+
+#define SkytetherLogPerf(phase_name, code_block) \
+  SkytetherStartTS(phase_name)                   \
+  code_block                                     \
+  SkytetherStopTS(phase_name)                    \
+  SkytetherLogTimestamps(phase_name)
+
+/* NOTE: for now this needs to always be available
 #if SKYTETHER_DEBUG
-  #define SkytetherInitLogger(logger_name) {                              \
-    *(SkytetherLogger(logger_name)) << "Logger initialized" << std::endl; \
-  }
-
-  #define SkytetherStartTS(phase_name) \
-    SteadyTS ts_start_##phase_name = steady_clock::now();
-
-  #define SkytetherStopTS(phase_name) \
-    SteadyTS ts_stop_##phase_name = steady_clock::now();
-
-  #define SkytetherLogTimestamps(phase_name) {                                           \
-    auto ts_diff = mohair::StringifyTSDiff(ts_start_##phase_name, ts_stop_##phase_name); \
-    *(SkytetherLogger()) << "["                                                          \
-                              << mohair::StringifyTS(ts_start_##phase_name) << ":µs"     \
-                      << ", " << mohair::StringifyTS(ts_stop_##phase_name)  << ":µs"     \
-                      << ", " << ts_diff                                    << ":µs"     \
-                   << "] |> " << #phase_name << std::endl                                \
-    ;                                                                                    \
-  }
-
-  #define SkytetherLogPerf(phase_name, code_block) \
-    SkytetherStartTS(phase_name)                   \
-    code_block                                     \
-    SkytetherStopTS(phase_name)                    \
-    SkytetherLogTimestamps(phase_name)
-
 #else
   #define SkytetherStartTS(phase_name)             {}
   #define SkytetherStopTS(phase_name)              {}
   #define SkytetherLogTimestamps(ts_name, log_msg) {}
   #define SkytetherLogPerf(phase_name, code_block) code_block
-
 #endif
+*/
 
 
 // ------------------------------
